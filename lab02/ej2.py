943f8d389e88f814fcd13a8637d93383d45e6ddc
Federico Virgolini <fvirgolini@gmail.com>
2021-04-24 20:19:26 -0300

    resolving ej2

---
+++ lab02/ej2.py
@@ -0,0 +1,14 @@
+from math import tan
+
+#insiso a)
+
+def fun_lab2ej2a(x):
+    return tan(x)-2*x
+
+#Se necesitaron 16 iteraciones para llegar al resultado
+
+#insiso b)
+
+def fun_lab2ej2b(x):
+    return x**2-3
+    
