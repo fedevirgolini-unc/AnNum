7d612b89dfaad120ece88e2d8a7751176fdc2fad
fedevirgolini-unc <federico.virgolini@mi.unc.edu.ar>
2021-06-20 19:54:26 -0300

    error de tipeo

--- Primer Parcial/Algo_Horner.py
+++ Primer Parcial/Algo_Horner.py
@@ -3,7 +3,7 @@ def horn(coefs, x):
     Implementación del algoritmo de Horner.
     Entradas:
         -coef: es un vector con los coeficientes del polinomio de mayor a menor grado
-        -x: es el valos de la coriable independiente
+        -x: es el valos de la variable independiente
     Salidas:
         -valor: resultado de evaluar el polinomio en x
     """
